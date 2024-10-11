10bfe715fee7658fbeff804b0321db7ae445a492
nahidrahat <nahidrahat007@gmail.com>
2024-10-11 16:05:36 +0600

    update

--- class_project_ostad/lib/main.dart
+++ class_project_ostad/lib/main.dart
@@ -23,6 +23,34 @@ class HomeActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
 
+
+      // body: Center(
+      //   child: Text('Hello Word this is a first app in ostad live class project ',
+      //     textAlign: TextAlign.center,
+      //     style: TextStyle(
+      //       color: Colors.black,
+      //       fontSize: 30,
+      //         // letterSpacing: 14,
+      //       wordSpacing: 5,
+      //       decoration: TextDecoration.overline,
+      //       overflow: TextOverflow.ellipsis
+      //   ),
+      //
+      //     ),
+      //
+      // ),
+
+      // body:
+      //     Image.asset('asset/image/emoji.jpg',
+      //       width: 50,
+      //       height: 50,
+      //       fit: BoxFit.cover,
+      //       alignment: Alignment.center,
+      //
+      //     ),
+
+
+
       body:  Center(
         child: InkWell(
           child: Text('text button'),

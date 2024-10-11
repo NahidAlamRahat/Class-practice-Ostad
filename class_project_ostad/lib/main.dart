596fea222159d88e9a8319c83daba4bf340b8ec4
nahidrahat <nahidrahat007@gmail.com>
2024-10-11 13:07:42 +0600

    update

--- class_project_ostad/lib/main.dart
+++ class_project_ostad/lib/main.dart
@@ -10,55 +10,27 @@ class Hello extends StatelessWidget {  @override
   Widget build(BuildContext context) {
 
   return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Class project'),
-      ),
-     /* body: Center(
-        child: Text('Hello Word this is a first app in ostad live class project ',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 30,
+    home: HomeActivity(),
+    );
+
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+      body:  Center(
+        child: InkWell(
+          child: Text('text button'),
         ),
-
-          ),
-
-      ),*/
-
-      // body:  
-      //     Image.asset('asset/image/emoji.jpg',
-      //       width: 50,
-      //       height: 50,
-      //       fit: BoxFit.cover,
-      //       alignment: Alignment.center,
-      //
-      //     ),
-      
-      
-      body:  Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-
-        children: [
-          Text('hello '),
-          Text('world'),
-
-      Image.asset('asset/image/emoji.jpg',
-              width: 300,
-              height:300,
-              fit: BoxFit.cover,
-              alignment: Alignment.center,
-
-            ),
-
-        ],
       ),
-      
-      
 
-    ),
-  );
+
+    );
 
   }
 }

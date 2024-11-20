e9bbfba98a21009118e3b1ce6635fd04a425ce1b
nahidrahat <nahidrahat007@gmail.com>
2024-11-21 01:10:11 +0600

    update

--- class_project_ostad/lib/main.dart
+++ class_project_ostad/lib/main.dart
@@ -1,112 +1,52 @@
 import 'package:class_project_ostad/rec_mian.dart';
-import 'package:class_project_ostad/screen1.dart';
-import 'package:class_project_ostad/screen2.dart';
-import 'package:class_project_ostad/screen3.dart';
-import 'package:class_project_ostad/screen4.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-/// ThemeData ,  Stack
+//Cupertino Design
 
-void main() {
-  runApp(Hello());
+void main(){
+  runApp(MyApp());
 }
 
-class Hello extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => HomeActivity(),
-        '/screen1': (context) => Stateful(),
-        '/screen2': (context) => Screen2(),
-        '/screen3': (context) => Screen3(),
-        '/screen4': (context) => Screen4(),
-      },
-
-      theme: ThemeData(
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            elevation: 20,
-            foregroundColor: Colors.pink,
-            side: BorderSide(color: Colors.black),
-
-          )
-        )
-      ),
-
+    return CupertinoApp(
       debugShowCheckedModeBanner: false,
-      home: HomeActivity(),
+       home: Cupertino(),
     );
   }
 }
 
-class HomeActivity extends StatelessWidget {
-  final String username;
-
-  HomeActivity({this.username = ''});
-
-  // CustomAlertDialog alertDialog = CustomAlertDialog();
-  TextEditingController firstController = TextEditingController();
-  TextEditingController secondController = TextEditingController();
-  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+class Cupertino extends StatelessWidget {
+  const Cupertino({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(backgroundColor: Colors.white, title: Text('Home')),
-      body: Center(
-        child: Column(
-          children: [
-            Text(username),
-            ElevatedButton(
-                onPressed: () {
-                 Navigator.pushNamed(context, '/screen1');
-
-                },
-                child: const Text('Go to StatefulWidget'),
-
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-
-              child: Stack(
-
-                children: [
-                  Container(
-                    color: Colors.amber,
-                    width: 100,
-                    height:100 ,
-                  ),
-
-                  Container(
-                    color: Colors.blue,
-                    width: 80,
-                    height:80 ,
-                  ),
-
-                  Positioned(
-                    
-                    width: 20,
-                    child: Container(
-                      color: Colors.pink,
-                      width: 60,
-                      height:60 ,
-                    ),
-                  )
-                ],
-              ),
-            )
-
-
-
-
-          ],
-        ),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle:Text('Cupertino-Design'),
+        leading: Icon(CupertinoIcons.add),
+        trailing: Icon(CupertinoIcons.arrow_up),
       ),
-    ); // Scaffold end.
+
+      child: Center(
+       child: Column(
+         children: [
+           CupertinoButton(child: Text('Enable'),
+               onPressed: (){},
+           ),
+
+
+           CupertinoButton.filled(child: Text('Enable'), onPressed: (){})
+
+         ],
+       ),
+    ),
+
+
+    );
   }
 }
+

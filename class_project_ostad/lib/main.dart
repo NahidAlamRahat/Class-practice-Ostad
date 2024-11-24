cefe2cc5c9523f4008000e2d30b5f51d17a83081
nahidrahat <nahidrahat007@gmail.com>
2024-11-25 02:24:24 +0600

    update

--- class_project_ostad/lib/main.dart
+++ class_project_ostad/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:class_project_ostad/rec_mian.dart';
 import 'package:class_project_ostad/screen2.dart';
 import 'package:flutter/material.dart';
 
-/// Wrap | MediaQuery | LayoutBuilder | OrientationBuilder
+/// FractionallySizedBox | Aspect Ratio | Expanded | Flexible | SafeArea | Tooltip
 
 void main() {
   runApp(MyApp());
@@ -15,61 +15,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Cupertino(),
+      home: HomeActivity(),
     );
   }
 }
 
-class Cupertino extends StatelessWidget {
-  const Cupertino({super.key});
-
+class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size mediaQuerySize = MediaQuery.sizeOf(context);
-    print(mediaQuerySize.height);
-    print(mediaQuerySize.width);
-    print(mediaQuerySize.flipped); /// er mane holo Width take Height a dibe Height take Width a dibe.
-    print(mediaQuerySize.longestSide);
-    print(mediaQuerySize.shortestSide);
-    print(mediaQuerySize.aspectRatio);
-
-    print(MediaQuery.of(context).devicePixelRatio);
-    print(MediaQuery.of(context).highContrast);
-
-    print(MediaQuery.of(context).orientation);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Module 11 class 1'),
         foregroundColor: Colors.white,
         backgroundColor: Colors.pink,
       ),
-      body: Center(
-        /*child: Wrap(
-          crossAxisAlignment: WrapCrossAlignment.center,
-          alignment: WrapAlignment.center,
-          spacing: 8,
-          runSpacing: 8,
-          // etar mane holo upore niche koto tuku spacing hobe
-          children: [
-            ElevatedButton(onPressed: () {}, child: const Text('Button1')),
-            ElevatedButton(onPressed: () {}, child: const Text('Button2')),
-            ElevatedButton(onPressed: () {}, child: const Text('Button3')),
-            ElevatedButton(onPressed: () {}, child: const Text('Button4')),
-            ElevatedButton(onPressed: () {}, child: const Text('Button5')),
-            ElevatedButton(onPressed: () {}, child: const Text('Button6')),
-            ElevatedButton(onPressed: () {}, child: const Text('Button7')),
-            ElevatedButton(onPressed: () {}, child: const Text('Button8')),
-          ],
-        ),*/
-
-        child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-             return Text('${constraints.maxHeight}');
-            },
-
+      body: SizedBox(
+        height: 300,
+        width: 300,
+        child: ColoredBox(
+          color: Colors.black,
+          child: FractionallySizedBox(
+            widthFactor: 0.5,
+            // প্রস্থ parent-এর ৫০%
+            heightFactor: 0.3,
+            // উচ্চতা parent-এর ৩০%
+            alignment: Alignment.bottomLeft,
+            /// eta tokhon kaj kore jokhon ei Widget ti  onno r ektar bitore thakbe.
+            ///
+            child: ColoredBox(
+              color: Colors.blue, // বক্সের রঙ
+            ),
+          ),
+        ),
       ),
-    )
     );
   }
 }
